8f4788cdc6699b0fbef59ee3cf079cf22e5f5240
Gil Alpert <alpertgil@gmail.com>
2024-08-16 21:25:48 +0300

    rename reader to product_reader and writer to command_writer

--- Server/api/server.py
+++ Server/api/server.py
@@ -7,8 +7,8 @@ from utils import readers, writers
 
 class Server:
     def __init__(self, writer: writers.Writer, reader: readers.Reader) -> None:
-        self._writer = writer
-        self._reader = reader
+        self._command_writer = writer
+        self._product_reader = reader
 
     def send_request(self, request: BaseRequest) -> str:
         """
@@ -17,9 +17,9 @@ class Server:
         :return: the id of the request and response - to look for
         """
         identifier = uuid.uuid4().hex
-        self._writer.write(identifier, request)
+        self._command_writer.write(identifier, request)
         return identifier
 
     def wait_for_response(self, identifier: str) -> BasicResponse:
-        for item in self._reader.read(identifier):
+        for item in self._product_reader.read(identifier):
             return BasicResponse(**item)
